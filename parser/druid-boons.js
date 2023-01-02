360f71364442da458747c25fdfed18c5907d0261
Lothrik <maximius@gmail.com>
2023-01-01 17:06:19 -0800

    Manually update Spirit Boons to 36331 data

--- parser/druid-boons.js
+++ parser/druid-boons.js
@@ -1,27 +1,27 @@
 let druidBoons = {
 	"Deer": {
-		"Prickleskin": `Gain 796 Thorns.`,
-		"Gift of the Stag": `Gain +10 Maximum Spirit.`,
-		"Wariness": `Gain x10% Damage Reduction against Elite Monsters.`,
-		"Advantageous Beast": `Reduce the duration of Control Impairing Effects by 15%.`
+		"Prickleskin": `Gain +796 Thorns.`,
+		"Gift of the Stag": `Gain +10 max Spirit.`,
+		"Wariness": `Take x10% Reduced Damage from Elites.`,
+		"Advantageous Beast": `Reduce the duration of incoming crowd control effects by 15%.`
 	},
 	"Eagle": {
-		"Scythe Talons": `+5% Critical Strike Chance.`,
-		"Iron Feather": `x10% Maximum Health.`,
-		"Swooping Attacks": `+10% Attack Speed.`,
-		"Avian Wrath": `+30% Critical Strike Damage.`
+		"Scythe Talons": `+5% Increased Critical Strike Chance`,
+		"Iron Feather": `x10% Maximum Health`,
+		"Swooping Attacks": `+10% Attack Speed`,
+		"Avian Wrath": `+30% Critical Strike Damage`
 	},
 	"Wolf": {
-		"Packleader": `Critical Strikes have a 20% chance to reset the cooldown of your companion's skills.`,
-		"Energize": `Lucky Hit: Up to a 15% chance to gain 10 Spirit.`,
-		"Bolster": `Defensive skills grant you 10% Base Life (#) as Fortify.`,
+		"Packleader": `Lucky Hit: Critical Strikes have up to a 20% chance to reset the cooldown of your Companion skills.`,
+		"Energize": `Lucky Hit: Dealing damage has up to a 15% chance to restore 10 Spirit.`,
+		"Bolster": `Fortify for 10% of your maximum Life when you use a Defensive skill.`,
 		"Calamity": `Extend the duration of Ultimate skills by 25%.`
 	},
 	"Snake": {
-		"Obsidian Slam": `Lucky Hit: Earth skills have up to a 15% chance to Stun for # seconds. If the target is already Stunned and isn't a Boss or Elite, your hits will Overpower instead.`,
-		"Overload": `Lucky Hit: Lightning damage has up to a 20% chance to cause the target to emit a static discharge, dealing 63.53% damage to nearby enemies.`,
-		"Masochistic": `Critical Strikes with Shapeshifting skills will recover 3% of your Base Life (#).`,
-		"Calm Before the Storm": `Lucky Hit: Nature skills have up to a 15% chance to reduce the cooldown of your Ultimate skills by 2 seconds.`
+		"Obsidian Slam": `Lucky Hit: Earth skills have up to a 15% chance to Stun enemies for # seconds. If the target is already Stunned and is not a boss, they Overpower the target instead.`,
+		"Overload": `Lucky Hit: Dealing Lightning damage has up to a 20% chance to cause the target to emit a static discharge, dealing 63.53% damage to surrounding enemies.`,
+		"Masochistic": `Critical Strikes with Shapeshifting skills heal you for 3% of your maximum Life.`,
+		"Calm Before the Storm": `Lucky Hit: Nature Magic skills have up to a 15% chance to reduce the cooldown of your Ultimate skill by 2 seconds.`
 	}
 };
 
