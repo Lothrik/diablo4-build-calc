51abef4c6eccdf6aa7992f02b7f8c53c4306b00a
Lothrik <maximius@gmail.com>
2023-01-15 12:04:42 -0800

    Further JSON viewer CSS improvements

--- parser/other/other.js
+++ parser/other/other.js
@@ -2,8 +2,34 @@ const buildNumber = 36331;
 
 var otherJSON = "";
 $.getJSON("build-" + buildNumber + ".json", null, successData => {
+	const root = $("#root");
 	const tree = jsonview.create(successData);
-	jsonview.render(tree, $("#root")[0]);
+	root.text("");
+	jsonview.render(tree, root[0]);
 	jsonview.expand(tree);
-	$(".json-key").html((_, oldHTML) => oldHTML.replace(/_/g, "<wbr>_"));
+
+	const jsonKeys = $(".json-key");
+	jsonKeys.html((index, oldHTML) => {
+		const newHTML = oldHTML.replace(/_/g, "<wbr>_");
+		const nextSibling = $(jsonKeys[index].nextElementSibling);
+		if (nextSibling.hasClass("json-size") || nextSibling.hasClass("json-separator")) {
+			const siblingHTML = nextSibling.prop("outerHTML");
+			nextSibling.remove();
+			return newHTML + siblingHTML;
+		} else {
+			return newHTML;
+		}
+	});
+
+	const caretIcons = $(".caret-icon");
+	caretIcons.html((index, oldHTML) => {
+		const nextSibling = $(caretIcons[index].nextElementSibling);
+		if (nextSibling.hasClass("json-key")) {
+			const siblingHTML = nextSibling.prop("outerHTML");
+			nextSibling.remove();
+			return oldHTML + siblingHTML;
+		} else {
+			return oldHTML;
+		}
+	});
 });
