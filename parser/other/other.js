16925defe72a0f75631b203236a4732ad214c6b9
Lothrik <maximius@gmail.com>
2023-01-15 10:43:28 -0800

    Improve JSON viewer CSS

--- parser/other/other.js
+++ parser/other/other.js
@@ -5,4 +5,5 @@ $.getJSON("build-" + buildNumber + ".json", null, successData => {
 	const tree = jsonview.create(successData);
 	jsonview.render(tree, $("#root")[0]);
 	jsonview.expand(tree);
+	$(".json-key").html((_, oldHTML) => oldHTML.replace(/_/g, "<wbr>_"));
 });
