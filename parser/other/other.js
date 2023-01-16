b4dd1a73e109865e3dfd3ddd1fdd5a5b68dd8dae
Lothrik <maximius@gmail.com>
2023-01-15 19:31:25 -0800

    Configure default JSON viewer state

--- parser/other/other.js
+++ parser/other/other.js
@@ -1,12 +1,35 @@
 const buildNumber = 36331;
 
+function traverse(node, callback) {
+	callback(node);
+	if (node.children.length > 0) {
+		node.children.forEach(child => {
+			traverse(child, callback);
+		});
+	}
+}
+
 var otherJSON = "";
-$.getJSON("build-" + buildNumber + ".json", null, successData => {
+$.getJSON("build-" + buildNumber + ".json", null, data => {
 	const root = $("#root");
-	const tree = jsonview.create(successData);
-	root.text("");
-	jsonview.render(tree, root[0]);
-	jsonview.expand(tree);
+	const tree = jsonview.create(data);
+
+	$("#loading").remove();
+
+	// recursively reduce `tree` node depth by 1
+	traverse(tree, node => node.depth -= 1);
+
+	// render and expand `tree`, skipping the root node
+	tree.children.forEach(data => {
+		jsonview.render(data, root[0]);
+		jsonview.expand(data);
+		data.children.forEach(child => {
+			jsonview.collapse(child);
+			traverse(child, node => {
+				if (node.depth > 1) node.isExpanded = true;
+			});
+		});
+	});
 
 	const jsonKeys = $(".json-key");
 	jsonKeys.html((index, oldHTML) => {
