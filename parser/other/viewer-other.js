3b2208f1a47ebb518514c8b104b68a8e901c53e5
Lothrik <maximius@gmail.com>
2023-03-02 20:02:49 -0800

    Delete viewer-other.js

--- parser/other/viewer-other.js
+++
@@ -1,8 +0,0 @@
-const buildNumber = 36331;
-
-var otherJSON = "";
-$.getJSON("build-" + buildNumber + ".json", null, successData => {
-	const tree = jsonview.create(successData);
-	jsonview.render(tree, $("#root")[0]);
-	jsonview.expand(tree);
-});
