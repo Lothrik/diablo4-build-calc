Lothrik/diablo4-build-calc

commit c24f15792220a93d0533c9b76957b24cf2bad095

Fix duplicate click/tap event bug

main.js
@@ -1034,27 +1034,13 @@ function redrawAllNodes(idleMode = false) {
 function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNodes.length, nodePosition = null) {
 	const scaleFactor = PIXI.settings.RESOLUTION >= 4 ? 1 : (newRenderScale > 0.8 ? 4 : newRenderScale > 0.5 ? 2 : 1) / PIXI.settings.RESOLUTION * newRenderScale;
 
-	let node;
+	let node = null;
 	if (pixiNodes.length > nodeIndex) {
 		node = pixiNodes[nodeIndex];
 		// skip node redraw if we already have the correct one displayed
 		if (node.scaleFactor == scaleFactor) return;
 		// destroy all existing node children if we need to replace them
 		while (node.children[0]) node.children[0].destroy(true);
-	} else {
-		node = new PIXI.Container();
-
-		node._renderHooked = node._render;
-		node._render = (...args) => {
-			if (node.stale) {
-				node.stale = false;
-				pixiEventQueue.push(() => redrawNode(node));
-			} else {
-				node._renderHooked(...args);
-			}
-		};
-
-		pixiNodes[nodeIndex] = pixiJS.stage.addChild(node);
 	}
 
 	let x = nodePosition == null ? nodeData.get("x") : nodePosition.x;
@@ -1216,6 +1202,52 @@ function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNod
 		nodeBorder.angle = 45;
 	}
 
+	if (node == null) {
+		node = new PIXI.Container();
+
+		node._renderHooked = node._render;
+		node._render = (...args) => {
+			if (node.stale) {
+				node.stale = false;
+				pixiEventQueue.push(() => redrawNode(node));
+			} else {
+				node._renderHooked(...args);
+			}
+		};
+
+		node
+			.on("mousedown", onDragStart)
+			.on("touchstart", onDragStart)
+			.on("mouseup", onDragEnd)
+			.on("mouseupoutside", onDragEnd)
+			.on("touchend", onDragEnd)
+			.on("touchendoutside", onDragEnd)
+			.on("mousemove", onDragMove)
+			.on("touchmove", onDragMove)
+			.on("mouseover", onMouseOver)
+			.on("mouseout", onMouseOut)
+			.on("tap", onMouseOver);
+
+		if ([PARAGON_BOARD, CODEX_OF_POWER, SPIRIT_BOONS, BOOK_OF_THE_DEAD].includes(groupName) || maxPoints <= 1) {
+			if (maxPoints != 0) {
+				node.cursor = "pointer";
+				node.interactive = true;
+				node
+					.on("click", () => handleToggleButton(node))
+					.on("tap", () => handleToggleButton(node));
+			}
+		} else if (groupName != undefined && maxPoints != 0) {
+			plusContainer
+				.on("click", () => handlePlusButton(node))
+				.on("tap", () => handlePlusButton(node));
+			minusContainer
+				.on("click", () => handleMinusButton(node))
+				.on("tap", () => handleMinusButton(node));
+		}
+
+		pixiNodes[nodeIndex] = pixiJS.stage.addChild(node);
+	}
+
 	node.cullable = true;
 	node.interactive = true;
 	node.stale = false;
@@ -1229,10 +1261,18 @@ function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNod
 	node.nodeIndex = nodeIndex;
 	node.scaleFactor = scaleFactor;
 
-	if ([PARAGON_BOARD, CODEX_OF_POWER, SPIRIT_BOONS, BOOK_OF_THE_DEAD, undefined].includes(groupName) || maxPoints <= 1) {
-		node.addChild(nodeBackground, nodeText, nodeBorder);
-	} else {
-		node.addChild(nodeBackground, nodeText, nodeText2, plusContainer, minusContainer, nodeBorder);
+	node.nodeDesc = nodeData.get("description");
+	if (node.nodeDesc != undefined && node.nodeDesc.length > 0 && requiredPoints == undefined) {
+		const nodeValues = node.nodeData.get("values");
+		if (nodeValues != undefined) {
+			nodeValues.forEach(nodeValue => {
+				if (nodeValue.length > 0){
+					node.nodeDesc = node.nodeDesc.replace(/{#}/, nodeValue);
+				} else {
+					node.nodeDesc = node.nodeDesc.replace(/{#}/, "#");
+				}
+			});
+		}
 	}
 
 	switch (nodeData.get("damageType")) {
@@ -1262,48 +1302,10 @@ function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNod
 			break;
 	}
 
-	node.nodeDesc = nodeData.get("description");
-	if (node.nodeDesc != undefined && node.nodeDesc.length > 0 && requiredPoints == undefined) {
-		const nodeValues = node.nodeData.get("values");
-		if (nodeValues != undefined) {
-			nodeValues.forEach(nodeValue => {
-				if (nodeValue.length > 0){
-					node.nodeDesc = node.nodeDesc.replace(/{#}/, nodeValue);
-				} else {
-					node.nodeDesc = node.nodeDesc.replace(/{#}/, "#");
-				}
-			});
-		}
-	}
-
-	node
-		.on("mousedown", onDragStart)
-		.on("touchstart", onDragStart)
-		.on("mouseup", onDragEnd)
-		.on("mouseupoutside", onDragEnd)
-		.on("touchend", onDragEnd)
-		.on("touchendoutside", onDragEnd)
-		.on("mousemove", onDragMove)
-		.on("touchmove", onDragMove)
-		.on("mouseover", onMouseOver)
-		.on("mouseout", onMouseOut)
-		.on("tap", onMouseOver);
-
-	if ([PARAGON_BOARD, CODEX_OF_POWER, SPIRIT_BOONS, BOOK_OF_THE_DEAD].includes(groupName) || maxPoints <= 1) {
-		if (maxPoints != 0) {
-			node.cursor = "pointer";
-			node.interactive = true;
-			node
-				.on("click", () => handleToggleButton(node))
-				.on("tap", () => handleToggleButton(node));
-		}
-	} else if (groupName != undefined && maxPoints != 0) {
-		plusContainer
-			.on("click", () => handlePlusButton(node))
-			.on("tap", () => handlePlusButton(node));
-		minusContainer
-			.on("click", () => handleMinusButton(node))
-			.on("tap", () => handleMinusButton(node));
+	if ([PARAGON_BOARD, CODEX_OF_POWER, SPIRIT_BOONS, BOOK_OF_THE_DEAD, undefined].includes(groupName) || maxPoints <= 1) {
+		node.addChild(nodeBackground, nodeText, nodeBorder);
+	} else {
+		node.addChild(nodeBackground, nodeText, nodeText2, plusContainer, minusContainer, nodeBorder);
 	}
 }
 function drawAllNodes() {

commit d7de346e11af7b7804c337a3c104f5bedd9f25c5

Fix various regressions from c24f157 + 4b21347

main.js
@@ -880,11 +880,10 @@ function handlePlusButton(curNode) {
 		if (getUnusedPoints(false) <= 0) return;
 
 		let validConnection = false;
-		const nodeConnections = curNode.nodeData.get("connections").values();
-		for (let i = 0, n = nodeConnections.length; i < n; i++) {
-			const connectedNode = nodeConnections[i];
-			for (let j = 0, n2 = pixiNodes.length; j < n2; j++) {
-				const parentNode = pixiNodes[j];
+		const nodeConnectionValues = curNode.nodeData.get("connections").values();
+		for (const connectedNode of nodeConnectionValues) {
+			for (let i = 0, n = pixiNodes.length; i < n; i++) {
+				const parentNode = pixiNodes[i];
 				if (parentNode.nodeName == connectedNode) {
 					if (parentNode.groupName == undefined) {
 						const requiredPoints = parentNode.nodeData.get("requiredPoints");
@@ -937,11 +936,10 @@ function handleMinusButton(curNode) {
 		}
 	} else {
 		let validConnection = false;
-		const nodeConnections = curNode.nodeData.get("connections").values();
-		for (let i = 0, n = nodeConnections.length; i < n; i++) {
-			const connectedNode = nodeConnections[i];
-			for (let j = 0, n2 = pixiNodes.length; j < n2; j++) {
-				const parentNode = pixiNodes[j];
+		const nodeConnectionValues = curNode.nodeData.get("connections").values();
+		for (const connectedNode of nodeConnectionValues) {
+			for (let i = 0, n = pixiNodes.length; i < n; i++) {
+				const parentNode = pixiNodes[i];
 				if (parentNode.nodeName == connectedNode) {
 					if (parentNode.groupName == undefined) {
 						const requiredPoints = parentNode.nodeData.get("requiredPoints");
@@ -989,12 +987,9 @@ function recursivePathValidation(startNode, recursionDepth = 0) {
 				return pixiNode.nodeData.get("requiredPoints") <= getAllocatedSkillPoints(pixiNode.nodeName);
 			} else if (pixiNode.nodeData.get("allocatedPoints") > 0) {
 				if (recursionDepth < MAX_RECURSION_DEPTH) {
-					const nodeConnections = pixiNode.nodeData.get("connections");
-					if (nodeConnections != undefined) {
-						const nodeValues = nodeConnections.values();
-						for (let j = 0, n2 = nodeValues.length; j < n2; j++) {
-							if (recursivePathValidation(nodeValues[j], recursionDepth + 1)) return true;
-						}
+					const nodeConnectionValues = pixiNode.nodeData.get("connections").values();
+					for (const connectedNode of nodeConnectionValues) {
+						if (recursivePathValidation(connectedNode, recursionDepth + 1)) return true;
 					}
 				}
 			}
@@ -1009,9 +1004,9 @@ function validateAllDependentNodes() {
 		const nodeConnections = pixiNode.nodeData.get("connections");
 		if (nodeConnections != undefined) {
 			let validConnection = false;
-			const nodeValues = nodeConnections.values();
-			for (let j = 0, n2 = nodeValues.length; j < n2; j++) {
-				validConnection = recursivePathValidation(nodeValues[j]);
+			const nodeConnectionValues = nodeConnections.values();
+			for (const connectedNode of nodeConnectionValues) {
+				validConnection = recursivePathValidation(connectedNode);
 				if (validConnection) break;
 			}
 			if (!validConnection) {
@@ -1156,11 +1151,17 @@ function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNod
 		plusContainer.cursor = "pointer";
 		plusContainer.interactive = true;
 		plusContainer.addChild(nodeText3);
+		plusContainer
+			.on("click", () => handlePlusButton(node))
+			.on("tap", () => handlePlusButton(node));
 
 		minusContainer = new PIXI.Container();
 		minusContainer.cursor = "pointer";
 		minusContainer.interactive = true;
 		minusContainer.addChild(nodeText4);
+		minusContainer
+			.on("click", () => handleMinusButton(node))
+			.on("tap", () => handleMinusButton(node));
 	}
 
 	const nodeBackground = new PIXI.Graphics();
@@ -1249,13 +1250,6 @@ function drawNode(nodeName, nodeData, groupName, branchData, nodeIndex = pixiNod
 					.on("click", () => handleToggleButton(node))
 					.on("tap", () => handleToggleButton(node));
 			}
-		} else if (groupName != undefined && maxPoints != 0) {
-			plusContainer
-				.on("click", () => handlePlusButton(node))
-				.on("tap", () => handlePlusButton(node));
-			minusContainer
-				.on("click", () => handleMinusButton(node))
-				.on("tap", () => handleMinusButton(node));
 		}
 
 		switch (nodeData.get("damageType")) {