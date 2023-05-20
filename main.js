cc066f0ce5d030d8a56304c68574848ec3580e69
Lothrik <maximius@gmail.com>
2023-05-19 20:41:00 -0700

    Add custom parsing to identify LHC behavior

--- main.js
+++ main.js
@@ -734,7 +734,7 @@ function handleClampButton(event) {
 	repositionTooltip();
 	resizeSearchInput();
 }
-const localVersion = "0.9.0.41428-8";
+const localVersion = "0.9.0.41428-9";
 var remoteVersion = "";
 var versionInterval = null;
 function handleVersionLabel(event) {
