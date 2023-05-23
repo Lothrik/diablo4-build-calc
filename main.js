cbf2874613857bb27c05fab1ec7ad3d8a46675ae
Lothrik <maximius@gmail.com>
2023-05-23 16:29:37 -0700

    Bump version number

--- main.js
+++ main.js
@@ -848,7 +848,7 @@ function handleClampButton(event) {
 	repositionTooltip();
 	resizeSearchInput();
 }
-const localVersion = "0.9.0.41428-20";
+const localVersion = "0.9.0.41428-21";
 var remoteVersion = "";
 var versionInterval = null;
 function handleVersionLabel(event) {
