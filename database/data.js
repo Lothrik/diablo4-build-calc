6a7d8850fbef72c3da8b5a2bd372c3f2937d8a46
Lothrik <maximius@gmail.com>
2023-07-18 14:29:37 -0700

    Fix minor paragon node bug

--- database/data.js
+++ database/data.js
@@ -1,5 +1,5 @@
 const buildNumber = 43333;
-const localVersion = `1.1.0.${buildNumber}-1`;
+const localVersion = `1.1.0.${buildNumber}-2`;
 var remoteVersion = "";
 var versionInterval = null;
 
--- main.js
+++ main.js
@@ -1542,7 +1542,7 @@ function handleClampButton(event) {
 function handleHistoryButton(event) {
 	window.open("./history/");
 }
-const localVersion = "1.1.0.43333-1";
+const localVersion = "1.1.0.43333-2";
 var remoteVersion = "";
 var versionInterval = null;
 function handleVersionLabel(event) {
